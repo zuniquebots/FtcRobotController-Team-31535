eab1a2f42850357ba818d34dd27262dc42828a3b
Rakhee Jain <youngcrittersacademy@gmail.com>
2025-10-07 19:59:28 -0700

    Created new file for Odometry

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ex1_HelloWorld11111111.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "HelloWorld")
+public class Ex1_HelloWorld11111111 extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello","Hello, World");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}

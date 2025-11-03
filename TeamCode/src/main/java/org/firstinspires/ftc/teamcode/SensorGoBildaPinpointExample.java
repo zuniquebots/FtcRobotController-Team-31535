f2f9e9768209b554556c74ad2bb82f166215e291
Arav Parakh <realaravparakh@gmail.com>
2025-11-02 20:09:21 -0800

    Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorGoBildaPinpointExample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SensorGoBildaPinpointExample.java
@@ -61,7 +61,6 @@ For support, contact tech@gobilda.com
  */
 
 @TeleOp(name="goBILDA Pinpoint Example", group="Linear OpMode")
-//@Disabled
 
 public class SensorGoBildaPinpointExample extends LinearOpMode {
 
